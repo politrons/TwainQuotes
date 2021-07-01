5279022f849ada533066a4c6cd605321f29cd2ca
politrons <politrons@gmail.com>
2021-07-01 12:54:03 +0200

    Add integration test

--- app/server.py
+++ app/server.py
@@ -34,11 +34,10 @@ def auth_user():
         command_dict = request.get_json()
         command = CreateTokenCommand(**command_dict)
         token = token_command_handler.create_token(command)
-        json_token = [{"token": f"{token}"}]
-        return json.dumps(json_token)
+        return json.dumps({"token": f"{token}"})
     except Exception as e:
         logging.error(f"Internal server error. Caused by {e}")
-        return json.dumps([{"message": "Internal server error. Caused by {}".format(e)}]), 500
+        return json.dumps({"message": "Internal server error. Caused by {}".format(e)}), 500
 
 
 @app.route('/quotes', methods=['GET'])
@@ -52,13 +51,13 @@ def get_quotes():
         return json.dumps(user_quotes_json)
     except token_command_handler.TokenExpiredError:
         logging.error(f"Error, the token has expired.")
-        return json.dumps([{"message": "Error, the token has expired."}]), 401
+        return json.dumps({"message": "Error, the token has expired."}), 401
     except TokenNotProvided:
         logging.error(f"Error, the token was not provided.")
-        return json.dumps([{"message": "Error, the token was not provided."}]), 401
+        return json.dumps({"message": "Error, the token was not provided."}), 401
     except Exception as e:
         logging.error(f"Internal server error. Caused by {e}")
-        return json.dumps([{"message": "Internal server error. Caused by {}".format(e)}]), 500
+        return json.dumps({"message": "Internal server error. Caused by {}".format(e)}), 500
 
 
 @app.route('/quotes/<quote_id>', methods=['GET'])
@@ -73,16 +72,16 @@ def get_quote_by_id(quote_id):
 
     except token_command_handler.TokenExpiredError:
         logging.error(f"Error, the token has expired.")
-        return json.dumps([{"message": "Error, the token has expired."}]), 401
+        return json.dumps({"message": "Error, the token has expired."}), 401
     except TokenNotProvided:
         logging.error(f"Error, the token was not provided.")
-        return json.dumps([{"message": "Error, the token was not provided."}]), 401
+        return json.dumps({"message": "Error, the token was not provided."}), 401
     except QuoteNotFoundException:
         logging.error(f"Error, the quote with id {quote_id} does not exist.")
-        return json.dumps([{"message": "Error, the quote with id {} does not exist.".format(quote_id)}]), 404
+        return json.dumps({"message": "Error, the quote with id {} does not exist.".format(quote_id)}), 404
     except Exception as e:
         logging.error(f"Internal server error. Caused by {e}")
-        return json.dumps([{"message": "Internal server error. Caused by {}".format(e)}]), 500
+        return json.dumps({"message": "Internal server error. Caused by {}".format(e)}), 500
 
 
 @app.route('/quotes/<quote_id>/share', methods=['GET'])
@@ -95,16 +94,16 @@ def create_share_url(quote_id):
         return json.dumps({"share_url": "/share/{}".format(shared_code)})
     except token_command_handler.TokenExpiredError:
         logging.error(f"Error, the token has expired.")
-        return json.dumps([{"message": "Error, the token has expired."}]), 401
+        return json.dumps({"message": "Error, the token has expired."}), 401
     except TokenNotProvided:
         logging.error(f"Error, the token was not provided.")
-        return json.dumps([{"message": "Error, the token was not provided."}]), 401
+        return json.dumps({"message": "Error, the token was not provided."}), 401
     except QuoteNotFoundException:
         logging.error(f"Error, the quote with id {quote_id} does not exist.")
         return json.dumps([{"message": "Error, the quote with id {} does not exist.".format(quote_id)}]), 404
     except Exception as e:
         logging.error(f"Internal server error. Caused by {e}")
-        return json.dumps([{"message": "Internal server error. Caused by {}".format(e)}]), 500
+        return json.dumps({"message": "Internal server error. Caused by {}".format(e)}), 500
 
 
 @app.route('/share/<share_url>', methods=['GET'])
@@ -117,10 +116,10 @@ def use_share_url(share_url):
 
     except ShareCodeNotFoundException:
         logging.error("Error, the share url is not correct or is not longer valid.")
-        return json.dumps([{"message": "Error, the share url is not correct or is not longer valid."}]), 404
+        return json.dumps({"message": "Error, the share url is not correct or is not longer valid."}), 404
     except Exception as e:
         logging.error(f"Internal server error. Caused by {e}")
-        return json.dumps([{"message": "Internal server error. Caused by {}".format(e)}]), 500
+        return json.dumps({"message": "Internal server error. Caused by {}".format(e)}), 500
 
 
 def get_token_from_header():
--- tests/test_server_integration.py
+++ tests/test_server_integration.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 from app.server import app
@@ -5,18 +6,41 @@ from app.server import app
 
 class TestServer(unittest.TestCase):
 
-    def test_home_page(self):
+    def test_auth_user(self):
         """
         GIVEN a Flask application for testing
         WHEN the '/auth' with username and password is requested (POST)
         THEN check that the response is a valid token
         """
-
-        # Create a test client using the Flask application configured for testing
-        with app.test_client() as c:
-            rv = c.post('/auth', json={
+        with app.test_client() as client:
+            client.environ_base['Content-Type'] = 'application/json'
+            rv = client.post('/auth', json={
                 'username': 'politrons', 'password': 'secret'
             })
+        json_token = json.loads(rv.get_data())
         self.assertTrue(rv.status_code == 200)
-        json_token = rv.data
-        self.assertTrue(json_token)
+        self.assertEqual(len(json_token), 1)
+        self.assertTrue(json_token["token"])
+
+    def test_get_quotes(self):
+        """
+        GIVEN a Flask application for testing
+        WHEN the '/auth' with username and password is requested (POST)
+        THEN check that the response is a valid token
+        """
+        with app.test_client() as client:
+            client.environ_base['Content-Type'] = 'application/json'
+            rv = client.post('/auth', json={
+                'username': 'politrons', 'password': 'secret'
+            })
+        json_token = json.loads(rv.get_data())
+        with app.test_client() as client:
+            client.environ_base['HTTP_AUTHORIZATION'] = 'Bearer ' + json_token["token"]
+            rv = client.get('/quotes')
+        self.assertTrue(rv.status_code == 200)
+        user_quote = json.loads(rv.get_data())
+        json_user_quote = json.loads(user_quote)
+
+        self.assertTrue(json_user_quote)
+        self.assertEqual(json_user_quote["user"], "Politrons")
+        self.assertEqual(len(json_user_quote["quotes"]), 31)
